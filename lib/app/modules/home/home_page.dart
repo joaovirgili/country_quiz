5d20c7728a41685ea84288a43ccd72a4e08e3291
JP <joao.virgili@carupi.com>
2021-01-04 19:57:09 -0300

    First commit

---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'home_controller.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends ModularState<HomePage, HomeController> {
+  //use 'controller' variable to access controller
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Países Quiz App"),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            RaisedButton(
+              onPressed: () {},
+              child: Text("Capital"),
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text("Bandeira"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
